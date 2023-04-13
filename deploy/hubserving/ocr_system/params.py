553a2ceec543fef3be0ba0fdc0e6518fabb023df
weifj <weifj@tuyuansu.com.cn>
2023-04-13 15:41:06 +0800

    fix: 恢复配置

--- deploy/hubserving/ocr_system/params.py
+++ deploy/hubserving/ocr_system/params.py
@@ -26,8 +26,7 @@ def read_params():
 
     #params for text detector
     cfg.det_algorithm = "DB"
-    #cfg.det_model_dir = "./inference/ch_PP-OCRv3_det_infer/"
-    cfg.det_model_dir = "./inference/ch_ppocr_mobile_v2.0_det_infer/"
+    cfg.det_model_dir = "./inference/ch_PP-OCRv3_det_infer/"
     cfg.det_limit_side_len = 960
     cfg.det_limit_type = 'max'
 
@@ -45,8 +44,7 @@ def read_params():
 
     #params for text recognizer
     cfg.rec_algorithm = "CRNN"
-    #cfg.rec_model_dir = "./inference/ch_PP-OCRv3_rec_infer/"
-    cfg.rec_model_dir = "./inference/ch_ppocr_mobile_v2.0_rec_infer/"
+    cfg.rec_model_dir = "./inference/ch_PP-OCRv3_rec_infer/"
 
     cfg.rec_image_shape = "3, 48, 320"
     cfg.rec_batch_num = 6
